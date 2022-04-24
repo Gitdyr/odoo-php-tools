c70dff6efd2dec0442842ef6a658b57bde6f1184
Kjeld Borch Egevang <kjeld@mail4us.dk>
2022-04-24 10:36:39 +0200

    Improve HtmlNode class

--- page.php
+++ page.php
@@ -1,121 +1,6 @@
 <?php
 
-class Node {
-    public $nodes = array();
-    public $attributes = array();
-    public $void = array(
-        'area',
-        'base',
-        'br',
-        'col',
-        'command',
-        'embed',
-        'hr',
-        'img',
-        'input',
-        'keygen',
-        'link',
-        'meta',
-        'param',
-        'source',
-        'track',
-        'wbr'
-    );
-    public $indent = '';
-
-    public function __construct($parent, $tag, $text ='')
-    {
-        $this->parent = $parent;
-        $this->tag = $tag;
-        $this->text = $text;
-    }
-
-    public function __call($name, $args)
-    {
-        $val = reset($args);
-        if (ctype_upper($name[0])) {
-            // New node
-            $tag = lcfirst($name);
-            $node = new Node($this, $tag, $val);
-            $node->indent = $this->indent.'  ';
-            $this->nodes[] = $node;
-            return $node;
-        } else {
-            // New attribute
-            $this->attributes[$name][] = $val;
-            return null;
-        }
-    }
-
-    public function First($tag)
-    {
-        $found = null;
-        foreach ($this->nodes as $node) {
-            if ($node->tag == $tag) {
-                $found = $node;
-                break;
-            }
-        }
-        return $found;
-    }
-    
-    public function Last($tag)
-    {
-        $found = null;
-        foreach ($this->nodes as $node) {
-            if ($node->tag == $tag) {
-                $found = $node;
-            }
-        }
-        return $found;
-    }
-    
-    public function Clone($text)
-    {
-        $node = clone $this;
-        $node->text = $text;
-        $this->parent->nodes[] = $node;
-        return $node;
-    }
-    
-    public function Display()
-    {
-        if ($this->attributes) {
-            printf("%s<%s", $this->indent, $this->tag);
-            foreach ($this->attributes as $key => $vals) {
-                printf(' %s="%s"', $key, implode(' ', $vals));
-            }
-            printf(">\n");
-        } else {
-            printf("%s<%s>\n", $this->indent, $this->tag);
-        }
-        if ($this->text) {
-            printf("%s  %s\n", $this->indent, $this->text);
-        }
-        foreach ($this->nodes as $node) {
-            $node->Display();
-        }
-        if (!in_array($this->tag, $this->void)) {
-            printf("%s</%s>\n", $this->indent, $this->tag);
-        }
-    }
-}
-
-
-class Html extends Node {
-    public function __construct()
-    {
-        parent::__construct(null, 'html');
-    }
-
-    public function Display()
-    {
-        header('Content-Type: text/html;charset=UTF-8');
-        print "<!DOCTYPE html>\n";
-        parent::Display();
-    }
-}
-
+include('htmlnode.php');
 
 class Page {
     public function Header()
@@ -129,11 +14,6 @@ class Page {
         $link = $head->Link();
         $link->rel('stylesheet');
         $link->href($bpath.'/css/bootstrap.min.css');
-        /*
-        $link = $head->Link();
-        $link->rel('stylesheet');
-        $link->href('css/style.css');
-        */
     }
 
     public function Item($ul, $name, $href = null)
@@ -186,7 +66,7 @@ class Page {
 
     public function Display()
     {
-        $this->html = new Html();
+        $this->html = new HtmlNode();
         $this->Header();
         $this->Body();
         $this->html->Display();
--- settings.php
+++ settings.php
@@ -9,6 +9,34 @@ class Settings extends Page
 {
     public function Contents($body)
     {
+        $form = $body->Form();
+
+        $div = $form->Div();
+        $div->class('card mx-auto');
+        $div->style('width: 40rem;');
+        $bdiv = $div->Div();
+        $bdiv->class('card-body');
+
+        $div = $bdiv->Div();
+        $div->class('row mb-3');
+        $label = $div->Label('User');
+        $label->class('col-sm-4 col-form-label');
+        $label->class('text-right');
+        $idiv = $div->Div();
+        $idiv->class('col-sm-8');
+        $input = $idiv->Input();
+        $input->class('form-control');
+
+        $div = $bdiv->Div();
+        $div->class('row mb-3');
+        $label = $div->Label('Password');
+        $label->class('col-sm-4 col-form-label');
+        $label->class('text-right');
+        $idiv = $div->Div();
+        $idiv->class('col-sm-8');
+        $input = $idiv->Input();
+        $input->class('form-control');
+
     }
 }
 
