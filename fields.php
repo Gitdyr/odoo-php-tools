d401f543e6431363a93a85bff569682b9cad2177
Kjeld Borch Egevang <kjeld@mail4us.dk>
2022-04-24 21:51:14 +0200

    Add fields exploring

---
+++ fields.php
@@ -0,0 +1,35 @@
+<?php
+
+include('page.php');
+
+class Fields extends Page
+{
+    public function HandlePost()
+    {
+        parent::HandlePost();
+        if ($_POST) {
+            $this->fields = ['string', 'type', 'help'];
+            $model = $this->Cookie('model');
+            $q = [];
+            $f = ['attributes' => $this->fields];
+            $res = $this->Connect();
+            if ($res) {
+                $this->response =
+                    $this->ExecKw($model, 'fields_get', $q, $f);
+            }
+        }
+    }
+
+    public function Contents($body, $title = '')
+    {
+        $div = parent::Contents($body, 'Fields');
+        $this->InputField($div, 'Model');
+        $this->SubmitButton($div);
+        $this->Result($body);
+    }
+}
+
+$page = new Fields();
+$page->Display();
+
+?>
